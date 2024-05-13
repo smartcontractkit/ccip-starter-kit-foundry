478395589951187fb9bf8bf97632049d1e6c8406
andrejrakic <andrej.rakic@smartcontract.com>
2024-05-13 15:09:14 +0200

    chore: Bump @chianlink/contracts-ccip package to v1.4

--- script/Example01.s.sol
+++ script/Example01.s.sol
@@ -5,7 +5,7 @@ import "forge-std/Script.sol";
 import "./Helper.sol";
 import {IRouterClient} from "@chainlink/contracts-ccip/src/v0.8/ccip/interfaces/IRouterClient.sol";
 import {Client} from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.sol";
-import {IERC20} from "@chainlink/contracts-ccip/src/v0.8/vendor/openzeppelin-solidity/v4.8.0/contracts/token/ERC20/IERC20.sol";
+import {IERC20} from "@chainlink/contracts-ccip/src/v0.8/vendor/openzeppelin-solidity/v4.8.3/contracts/token/ERC20/IERC20.sol";
 
 contract CCIPTokenTransfer is Script, Helper {
     function run(
